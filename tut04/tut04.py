915000521d274861b7195c6f9230c3bbb73ce57b
uneednotknow <prkhushi2002@gmail.com>
2023-04-14 19:54:41 +0530

    Solution Code for Assignment4

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -1,161 +1,279 @@
+# Importing the required models
+import queue  
+import threading  
+import time 
+from os.path import exists 
 
-from datetime import datetime
-start_time = datetime.now()
+#Definng an object class for Router
+class Router: 
 
-#Help https://youtu.be/H37f_x4wAC0
-def octant_longest_subsequence_count_with_range():
-    import pandas as pd
-    try:
-        data=pd.read_excel("input_octant_longest_subsequence_with_range.xlsx")
-    except FileNotFoundError:
-        print("Incorrect file name")  
-    else:
+    INF = 1e18  
 
-        ##############################
-        #Creating the columns in the excel sheet
-        data["Octant"]="" 
-        data[""]=""
-        data["count"]=""
-        data["Longest Subsequence Length"]=""
-        data["Count"]=""
-        data["  "]=""
-        data["octant"]=""
-        data["Length of longest subsequence"]=""
-        data["Count of longest subsequence"]=""
-        values=["+1","-1","+2","-2","+3","-3","+4","-4"]
-        for i in range(0,8,1):
-            data.at[i,"count"]=values[i]
-        ##############################
-        lastval=data.index[-1]  #To calculate the last index, when index starts from 0 
-        ##############################################
-        #The following lines of code would check the numbers in the 3 columns and determine the octant values
-        #based on the conditional statements provided
-        #Calculating the octant value follows the same logic as described in the explanatory video
-        for i in range(0,lastval+1,1):
-            u=data["U'=U-U Avg"][i]
-            v=data["V'=V-V Avg"][i]
-            w=data["W'=W-W Avg"][i]
-            if u>0:
-                if v>0:
-                    if w>0:
-                        
-                        data.at[i,'Octant']="+1"
-                    else:
-                        
-                        data.at[i,'Octant']="-1"
-                else:
-                    if w>0:
-                        
-                        data.at[i,'Octant']="+4"
-                    else:
-                        
-                        data.at[i,'Octant']="-4"
+    #Initialising router object
+    def __init__(R, router_id):  
+        R.id = router_id  # Assigning the router ID
+        # Creating an empty set to store the changes done in the routing table
+        R.changes = set()
+        # Creating an empty dictionary to store the neighbours and edge costs -> dict[Router, int]
+        R.neighbours_list = {}
+        # Creating an empty list to store the routing table entries -> list[list[Router, Router, int]]
+        R.routing_table = []
+        # Creating a queue object to store the incoming routing tables.
+        R.queue = queue.Queue()
+
+    # Adding a neighbour to the router
+    def add_neighbours(R, neighbour, edge_cost):
+
+        #Checking if the neighbor node is actually self and exiting
+        if R == neighbour:  
+
+            print("Loop is not allowed.")  
+            exit(0) 
+        #Else, adding the neighbour and edge cost to the neighbours dictionary
+        R.neighbours_list[neighbour] = edge_cost
+
+    # Initializing the routing table of the router
+    def initialise_routing_table(R, routers):
+
+        #For all router instances, do:
+        for router in routers:  
+
+            # Check if the router is in the neighbour list of the current router
+            if R.neighbours_list.get(router):
+
+                # If yes, add the entry
+                # Using the edge dist from the neighbour list
+                R.routing_table.append([router, router, R.neighbours_list.get(router)])
+            elif router == R:  
+
+                # Add a routing table entry
+                # Using distance 0, as self 
+                R.routing_table.append([router, router, 0])
             else:
-                if v>0:
-                    if w>0:
-                        data.at[i,'Octant']="+2"
-                    else:
-                        data.at[i,'Octant']="-2"
-                else:
-                    if w>0:
-                        data.at[i,'Octant']="+3"
-                    else: 
-                        data.at[i,'Octant']="-3"
-        
-        
-        count=[0,0,0,0,0,0,0,0] #creating an empty array to store the counted values of consecutive occurrences
-        large=[0,0,0,0,0,0,0,0] #creating another empty array to store the largest consecutively counted occurrences
-        pos=0
-        for i in range(0,lastval,1):
-            #goes to a value at a particular time and checks if the next occurring element is same as the current one
-            if(data['Octant'][i]==data['Octant'][i+1]):     
-                for j in range(0,8,1):                      
-                    if (data['Octant'][i]==values[j]):
-                        #keeps counting till the next element is same as the previous one                              
-                        count[j]+=1
-                        pos=j
-            else:
-                #after one point the next element will not be same as the previous one
-                #in that case we'll be checking if the counted value is largest value counted yet 
-                #once the largest counted value of the octant is added to large, count is set to zero again  
-                                                        
-                if (count[pos]>large[pos]):                 
-                    large[pos]=count[pos]                   
-                    count[pos]=0      
 
-                #in case the counted value is smaller than or equal to the previous largest counted value then 'large' remains the same as before
-                
-                else:
-                    large[pos]=large[pos]                   
-                    count[pos]=0          
-                    
-        
-        #the previous blocks counted the largest consecutive occerrence of a particular octant value
-        #but the counted value will be one less than the actual value because the initial value of 'count' was set to zero
-        #hence every octant element that exists, is given an additional count value in the code given below
-        for j in range(0,8,1):
-            for i in range(0,lastval,1):
-                if (data['Octant'][i]==values[j]):
-                    large[j]+=1
-                    break
-        
-        #code to count how many largest occurrences present for each octant value
-        count2=[0,0,0,0,0,0,0,0]              
-        for j in range(0,8,1):                   
-            for i in range(0,lastval-large[j]+1,1):
-                supercount=0
-                for k in range(0,large[j],1):
-                    if(data['Octant'][i+k]==values[j]):
-                        supercount+=1
-                if(supercount==large[j]):
-                    count2[j]+=1
-        #code to insert the final values to the excel sheet
-        for i in range(0,8,1):
-            data.at[i,"Longest Subsequence Length"]=large[i]
-            data.at[i,"Count"]=count2[i]
-        
+                # Add a routing table entry for all other routers in the network
+                # Because not approachable, using distance Inf.
+                R.routing_table.append([router, None, R.INF])
 
-        timefromtopos=1
-        for i in range(0,8,1):
+    # Method to print the routing table of a router
+    def print_routing_table(R, lock):
+        # Acquire the lock before executing so as to prevent simultaneous read-write
+        lock.acquire()
 
-            #Here we write the octant values as headings for each block in the excel sheet
-            #We also add the headings "Time", "To" and "From" to the blocks for each octant
-            data.at[timefromtopos-1,"octant"]=values[i]
-            data.at[timefromtopos,"octant"]="Time"
-            data.at[timefromtopos-1,"Length of longest subsequence"]=large[i]
-            data.at[timefromtopos,"Length of longest subsequence"]="From"
-            data.at[timefromtopos,"Count of longest subsequence"]="To"
-            data.at[timefromtopos-1,"Count of longest subsequence"]=count2[i]
+        print("Printing the router table from router: ", R.id)
+        print("Dest. Router  | Cost       | Via Router  |")
+
+        # Iterate through each element of the routing table
+        for element in R.routing_table:
+
+            # Extract the destination router ID 
+            dest_router = element[0].id
+
+            # If the destination router is in the set of recently changed routers, do
+            if element[0].id in R.changes:
+                R.changes.remove(element[0].id)
             
-            anotherpos=1
-            for j in range(0,lastval,1):
-                newcount=1
-                if(values[i]==data["Octant"][j]):   #this code checks the initial and final timestamp of the largest consecutive occerrence of an octant value
-                    for k in range(1,large[i],1):   #it goes to the octant value, checks if the next 'n' consecutive numbers are same as the current number
-                        if(large[i]<=(lastval-j)):  #where 'n' is the largest consecutive occerrence calculated previously
-                            if(data["Octant"][j]==data["Octant"][j+k]):
-                                newcount+=1
-                if(newcount==large[i]):
-                          #writes the timestamp to the excel sheet
-                          data.at[timefromtopos+anotherpos,"Length of longest subsequence"]=data["Time"][j]
-                          data.at[timefromtopos+anotherpos,"Count of longest subsequence"]=data["Time"][j+large[i]-1]
-                          anotherpos+=1  
-            timefromtopos+=2+count2[i]
-        data.to_excel("output_octant_longest_subsequence_with_range.xlsx",index=False)
+            # Print the routing table entry, with proper spacing and alignment
+            print(f" {dest_router:<13} "
+                + f" {element[2] if element[2] != R.INF else 'INF' :<16} "
+                + f" {(element[1].id if element[1] else '--'):<15} ")
+
+        print("\n")
+        # Release the lock
+        lock.release()
+
+    def forward_routing_table(R):
+
+        # For each neighbor in the router's list of neighbors
+        for neighbour in R.neighbours_list.keys():
+
+            # Add this router's instance to the neighbor's message queue
+            neighbour.queue.put(R)
+
+    def update_routing_table(R):
+        # Set a flag to false indicating that no changes have been made to the routing table
+        is_there_a_change = False
+
+        # Loop forever until a break statement is reached
+        while True:
+            try:
+                # Get the next item in the router's message queue
+                neighbour = R.queue.get(block=False)
+
+                # Determine the cost to the neighbor and its routing table
+                cost_to_neighbour = R.neighbours[neighbour]
+                routing_table_of_neighbour = neighbour.routing_table
+
+                # Loop through the router's routing table and update entries as necessary
+                for index, entry in enumerate(R.routing_table):
+                    # New cost is the cost to the neighbour (say X) plus the cost from X to any other router (say Y)
+                    new_cost = cost_to_neighbour + \
+                        routing_table_of_neighbour[index][2]
+                    # If new cost is less than the cost from the current router to Y, use B-F equation
+                    if new_cost < entry[2]:
+                        is_there_a_change = True
+                        R.changes.add(entry[0].id)
+                        R.routing_table[index] = [
+                            entry[0], neighbour, new_cost]
+            except:
+                # If the router's message queue is empty, break out of the loop
+                break
+
+        # Return the flag indicating whether a change was made to the routing table
+        return is_there_a_change
 
 
+def parse_input():
+    number_of_routers = 0               # Initialize the number of routers to zero
+    routers = []                        # Create an empty list for routers
+    # Create an empty dictionary for mapping router IDs to their index in the routers list
+    routers_map = {}
 
-from platform import python_version
-ver = python_version()
+    with open('topology.txt') as f:        # Open the file for reading
 
-if ver == "3.8.10":
-    print("Correct Version Installed")
-else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+        # Read the first file
+        # Converting the string to an integer
+        number_of_routers = int(f.readline().strip())
+        #Printing the number of routers
+        print ("Number of routers connected are: ",number_of_routers)
+
+        #Reading the second file 
+        #Printing the mapping
+        print("The mapping is: ")
+        for index, router_id in enumerate(f.readline().strip().split(" ")):
+            
+            # Create a Router object for each router ID and add it to the routers list
+            routers.append(Router(router_id))
+
+            # Add the router ID and its index in a map
+            routers_map[router_id] = index
+
+            #Print the mapping
+            print(router_id, ",", index)
+        print("\n")
+
+        for line in f.readlines():       
+            
+            # Iterate through the remaining files of the file
+            # Remove any white space from the file
+            line = line.strip()
+
+            # If the current edge reads, EOF, do
+            if line == "EOF": 
+                break;                      
+
+            # Split the file for further operations
+            line = line.split(" ")
+
+            # Getting the index of the first node 
+            first_node = routers_map[line[0]]
+            # Getting the index of the second node 
+            second_node = routers_map[line[1]]
+            # Converting the third string to an integer
+            dist = int(line[2])
+
+            # Adding the routers as each other's neighbours
+            routers[first_node].add_neighbours(routers[second_node], dist)
+            routers[second_node].add_neighbours(routers[first_node], dist)
+
+    # Returning the number of routers and the routers list as a tuple
+    return number_of_routers, routers
 
 
-octant_longest_subsequence_count_with_range()
+# Initializing the routing table for each router object
+def initialise_routers(routers):
 
-#This shall be the last lines of the code.
-end_time = datetime.now()
-print('Duration of Program Execution: {}'.format(end_time - start_time))
+    # For all router instances in routers, do
+    for r in routers:
+        r.initialise_routing_table(routers)
+
+
+# Printing the routing table for all routers
+def print_tables(routers, lock):
+
+    print("## New iteration. ##")
+    # For all routers, do
+    for router in routers:
+        router.print_routing_table(lock)
+    print("\n")
+
+
+# Defining all the function a thread needs to perform to facilitate information exchange
+def thread_function(router, lock, routers):
+
+    # Setting the waiting time between each iteration to 2 seconds
+    waiting_time = 2
+
+    # Setting the flag to True indicating that a change is initially present in the routing table
+    is_there_a_change = True
+
+    # Setting the flag to True indicating that the function can continue
+    can_continue = True
+
+    # Continue the simulation while the flag can_continue is True
+    while can_continue:
+
+        # If there is a change in the routing table, forward the routing table
+        if is_there_a_change:
+            router.forward_routing_table()
+
+        # Update the routing table and check if there is a change
+        is_there_a_change = router.update_routing_table()
+
+        # If there is a change in the routing table, print the routing table
+        if is_there_a_change:
+            router.print_routing_table(lock)
+
+        # Wait for the specified time before the next iteration
+        time.sleep(waiting_time)
+
+        # Set the flag can_continue to False
+        can_continue = False
+
+        # Check if any router has any incoming routing table in its queue, if so, set the can_continue flag to True
+        for each_router in routers:
+            can_continue |= not each_router.queue.empty()
+
+
+def Main():
+
+    # Creating a threading lock
+    #for proper resource allocation
+    lock = threading.Lock()
+
+    # Calling function to read input from file
+    number_of_routers, routers = parse_input()
+
+    # Calling function to initialise each router's routing table
+    initialise_routers(routers)
+
+    # Printing the initial routing table for all routers
+    print_tables(routers, lock)
+
+    # Create a list threads
+    #holds all the threads
+    threads = []
+
+    # For all routers, do
+    for router in routers:
+
+        # Creating a new thread for each router and passing suuitable arguments
+        thread = threading.Thread(target=thread_function, args=(router, lock, routers))
+        # Start the function
+        thread.start()
+        # Adding the newly formed thread to the list
+        threads.append(thread)
+
+    # For all threads
+    for thread in threads:
+
+        # Wait for each thread to finish executing
+        thread.join()
+
+    # Again calling the function to print the final routing table for all routers
+    print_tables(routers, lock)
+
+
+if __name__ == "__main__":
+    Main()
+
